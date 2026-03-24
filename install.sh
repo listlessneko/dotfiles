343d402dd445f3575bd31ca279dc08ed9b75c1a6
listlessneko <98135875+listlessneko@users.noreply.github.com>
2026-03-24 10:57:57 -0700

    Added install script for ripgrep

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# Install Neovim 0.10.0 if not already installed or wrong version
+
+#Install ripgrep
+if ! command -v rg &> /dev/null; then
+  sudo apt install -y ripgrep
+fi
+
+# Install Neovim 0.10.0
 if ! command -v nvim &> /dev/null || [[ $(nvim --version | head -1) != *"v0.10.0"* ]]; then
   curl -LO https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz
   tar -xzf nvim-linux64.tar.gz
@@ -14,12 +20,12 @@ ln -sf ~/.config/coderv2/dotfiles/nvim ~/.config/nvim
 # Symlink bashrc
 ln -sf ~/.config/coderv2/dotfiles/.bashrc ~/.bashrc
 
-# Install tmux if not already installed or wrong version
+# Install tmux 3.5a
 if ! command -v tmux &> /dev/null || [[ $(tmux -V) != *"3.5a"* ]]; then
   sudo apt install -y tmux
 fi
 
-# Install TPM if not already installed
+# Install TPM
 if [ ! -d ~/.tmux/plugins/tpm ]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
